4754a4e1697af3015692b619786f1ceac81f3dcc
BuildTools <kring.basti.2004@gmail.com>
2024-08-11 22:35:19 +0200

    number-input component

--- src/app/modules/shared/components/number-input/number-input.component.ts
+++ src/app/modules/shared/components/number-input/number-input.component.ts
@@ -1,11 +1,17 @@
-import { Component, Input, OnInit } from '@angular/core';
-import { ControlsOf } from '../../models/controls-of.type';
-import { ControlValueAccessor, NG_VALUE_ACCESSOR } from '@angular/forms';
+import {Component, ElementRef, EventEmitter, Input, OnChanges, OnInit, Output, ViewChild} from '@angular/core';
+import {ControlValueAccessor, FormsModule, NG_VALUE_ACCESSOR, ReactiveFormsModule,} from '@angular/forms';
+import {BehaviorSubject} from "rxjs";
+import {NgChanges} from "../../models/ng-changes.type";
+import {AsyncPipe} from "@angular/common";
 
 @Component({
   selector: 'basalt-number-input',
   standalone: true,
-  imports: [],
+  imports: [
+    FormsModule,
+    ReactiveFormsModule,
+    AsyncPipe
+  ],
   templateUrl: './number-input.component.html',
   styleUrl: './number-input.component.scss',
   providers: [
@@ -16,41 +22,86 @@ import { ControlValueAccessor, NG_VALUE_ACCESSOR } from '@angular/forms';
     }
   ]
 })
-export class NumberInputComponent implements ControlValueAccessor, OnInit {
+export class NumberInputComponent implements ControlValueAccessor, OnChanges, OnInit {
+  @ViewChild('inputElement') public inputElement!: ElementRef<HTMLInputElement>;
+
   @Input() public min?: number;
   @Input() public max?: number;
   @Input() public step?: number;
-  public disabled = false;
+  @Input() public disabled = false;
+  @Input() public value: number = 0;
 
-  private value: number = 0;
-  private onTouched!: () => void;
-  private onChange!: (value: number) => void
+  @Output() public focusLost = new EventEmitter<void>;
+  @Output() public valueChange = new EventEmitter<number>;
+  @Output() public submit = new EventEmitter<void>;
+
+  public value$ = new BehaviorSubject<number>(0);
+  public disabled$ = new BehaviorSubject<boolean>(false);
+
+  public touch?: () => void;
+  public change?: (value: number) => void
 
   public ngOnInit(): void {
-
-
+    if (this.min && this.value$.value < this.min) this.value$.next(this.min);
+    if (this.max && this.value$.value > this.max) this.value$.next(this.max);
   }
 
-  public registerOnChange(fn: (value: number) => void): void {
-    this.onChange = fn;
-  }
-
-  public registerOnTouched(fn: () => void): void {
-    this.onTouched = fn
-  }
-
-  public setDisabledState(isDisabled: boolean): void {
-    this.disabled = isDisabled;
+  public ngOnChanges(changes: NgChanges<NumberInputComponent>): void {
+    if (changes.disabled) this.disabled$.next(changes.disabled.currentValue);
+    if (changes.value) this.writeValue(changes.value.currentValue);
   }
 
   public writeValue(value: number): void {
-    if(value !== undefined && this.value !== value){
-      if (this.min && value < this.min) value = this.min;
-      if (this.max && value < this.max) value = this.max;
+    this.value$.next(value);
+  }
 
-      this.value = value
-      this.onChange(value)
-      this.onTouched()
+  public registerOnChange(fn: (value: number) => void): void {
+    this.change = (value: number): void => fn(value);
+  }
+
+  public registerOnTouched(fn: () => void): void {
+    this.touch = fn
+  }
+
+  public setDisabledState(isDisabled: boolean): void {
+    this.disabled$.next(isDisabled);
+  }
+
+  public input(target: EventTarget | null) {
+    if (!target) return;
+    const {value: rawValue} = target as HTMLInputElement;
+    if (rawValue == '') {
+      let newVal = 0;
+      if (this.min && newVal < this.min) newVal = this.min;
+      if (this.max && newVal > this.max) newVal = this.max;
+      this.fieldChange(newVal);
     }
+    let value = +rawValue;
+
+    if (this.min && value < this.min) {
+      value = this.min
+      this.fieldChange(value);
+    }
+    if (this.max && value > this.max) {
+      value = this.max;
+      this.fieldChange(value)
+    }
+    if (this.step && +rawValue % this.step !== 0) {
+      value -= value % this.step;
+      const newValue = +rawValue - +rawValue % this.step;
+      this.fieldChange(newValue)
+    }
+
+    this.change?.(value);
+    this.valueChange.emit(value);
+    this.writeValue(value);
+  }
+
+  public fieldChange(num: number): void {
+    this.inputElement.nativeElement.value = `${num}`
+  }
+
+  public onSubmit(): void {
+    this.submit.emit();
   }
 }
---
+++ src/app/modules/shared/models/ng-changes.type.ts
@@ -0,0 +1,9 @@
+import { SimpleChange, SimpleChanges } from '@angular/core';
+
+export type NgChanges<T> = {
+  [K in keyof T]?: NgChange<T[K]>
+} & SimpleChanges
+
+type NgChange<T> = {
+  [K in keyof SimpleChange]: SimpleChange[K] extends any ? T : SimpleChange[K]
+}
