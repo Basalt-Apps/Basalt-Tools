ae7d4ebded584075411eac054eb78d883081ea8e
BuildTools <kring.basti.2004@gmail.com>
2024-08-11 22:36:16 +0200

    cleanup 2

--- src/app/modules/home/components/app-thumbnail/app-thumbnail.component.ts
+++ src/app/modules/home/components/app-thumbnail/app-thumbnail.component.ts
@@ -1,4 +1,4 @@
-import {Component, Input, OnInit} from '@angular/core';
+import {Component, Input} from '@angular/core';
 import { BasaltRoute } from '../../../shared/models/baasti-route.model';
 
 @Component({
