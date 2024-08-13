9072c120396b6ba896a6f8bd9e4ee3a4ec1a651f
BuildTools <kring.basti.2004@gmail.com>
2024-08-13 18:55:35 +0200

    patch

--- src/app/modules/spell-point-tracker/services/spt-form-helper.service.ts
+++ src/app/modules/spell-point-tracker/services/spt-form-helper.service.ts
@@ -21,7 +21,7 @@ export class SPTFormHelperService {
         nonNullable: true,
         validators: [
           Validators.required,
-          Validators.min(0),
+          Validators.min(4),
           Validators.max(999),
         ],
       }),
@@ -29,7 +29,7 @@ export class SPTFormHelperService {
         nonNullable: true,
         validators: [
           Validators.required,
-          Validators.min(0),
+          Validators.min(4),
           Validators.max(999),
         ],
       }),
